3e69df1940061a2c2d55561225fb3c5914dc220b
Andrii <ckifix@icloud.com>
2023-02-12 02:59:06 +0200

    Initial Commit

---
+++ SplashSceen/SplashSceenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashSceenApp.swift
+//  SplashSceen
+//
+//  Created by Andrii Zlenko on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SplashSceenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
